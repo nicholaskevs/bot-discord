5fbc165fe13426696ef65f66664c85ef9fddaa9c
nicholaskevs <nekoman.hunter.tech@gmail.com>
2023-02-09 01:23:17 +1100

    Fix snowflakes id lenght in db schema

--- config/schema/20230207-Initial schema.sql
+++ config/schema/20230207-Initial schema.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE `channels` (
 	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`discord_id` VARCHAR(18) NOT NULL,
+	`discord_id` VARCHAR(25) NOT NULL,
 	`name` VARCHAR(100) NOT NULL,
 	`type` TINYINT(2) NOT NULL,
 	PRIMARY KEY (`id`)
@@ -10,7 +10,7 @@ CREATE TABLE `channels` (
 CREATE TABLE `messages` (
 	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`channel_id` INT(11) UNSIGNED NOT NULL,
-	`discord_id` VARCHAR(18) NOT NULL,
+	`discord_id` VARCHAR(25) NOT NULL,
 	`author` VARCHAR(100) NOT NULL,
 	`content` VARCHAR(2000) NOT NULL,
 	`forwardedOn` DATETIME NULL,
