350045afc1504fdb106380611c5c81f4b1f03fa0
nicholaskevs <nekoman.hunter.tech@gmail.com>
2023-02-13 02:09:02 +1100

    Don't process bot own messages

--- src/main.php
+++ src/main.php
@@ -3,6 +3,7 @@
 require dirname(__DIR__).'/vendor/autoload.php';
 require dirname(__DIR__).'/config/cons.php';
 
+use Discord\Builders\MessageBuilder;
 use DiscordBot\Lib\Manager;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
@@ -19,12 +20,16 @@ $bot->on('ready', function (Discord $bot) {
 	
 	Manager::updateChannels($bot);
 	
-	$bot->on(Event::MESSAGE_CREATE, function (Message $message) {
-		Manager::saveMessage($message);
+	$bot->on(Event::MESSAGE_CREATE, function (Message $message, Discord $bot) {
+		if($message->author->id != $bot->id) {
+			Manager::saveMessage($message);
+		}
 	});
 	
-	$bot->on(Event::MESSAGE_UPDATE, function (Message $message) {
-		Manager::saveMessage($message);
+	$bot->on(Event::MESSAGE_UPDATE, function (Message $message, Discord $bot) {
+		if($message->author->id != $bot->id) {
+			Manager::saveMessage($message);
+		}
 	});
 });
 
